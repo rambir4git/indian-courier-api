d2961349679b0b15209c97a2ac81a4ebb0e4cd21
Rambir <rambir.git@gmail.com>
2020-12-29 15:00:15 +0530

    commit

--- api/xpressbees.js
+++ api/xpressbees.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios')
 const tabletojson = require('tabletojson');
 
 router.get('/:awb', (req, res) => {
     let trackingId = req.params.awb
     return axios.get(`https://www.xpressbees.com/track?isawb=Yes&trackid=${trackingId}`)
     .then(response => {
+        console.log(response);
         if(!response.data.data[0].scans){
             return res.json({ result: `Invalid Tracking No. ${trackingId}` })
         }
