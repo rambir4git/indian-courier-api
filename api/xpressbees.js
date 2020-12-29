595ba2af4f2812dfac466f78a85b7cd25ad16565
Rambir <rambir.git@gmail.com>
2020-12-29 15:36:24 +0530

    commit

--- api/xpressbees.js
+++ api/xpressbees.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const tabletojson = require('tabletojson');
-
-let url = 'http://www.xpressbees.com/track?isawb=Yes&trackid=';
+const axios = require('axios')
 
 router.get('/:awb', (req, res) => {
     let trackingId = req.params.awb
-    tabletojson.convertUrl(url + trackingId)
-    .then(tablesAsJson => {
-        console.log(tablesAsJson.length)
-        return (!tablesAsJson || !tablesAsJson.length || tablesAsJson.length == 1) ? res.json({ result: `Invalid Tracking Id ${trackingId}` }) : res.json({ result: tablesAsJson[2] })
+    return axios.get(`http://www.xpressbees.com/track?isawb=Yes&trackid=${trackingId}`)
+    .then(response => {
+        console.log(response);
+        if(!response.data.data[0].scans){
+            return res.json({ result: `Invalid Tracking No. ${trackingId}` })
+        }
+        let final = response.data.data[0].scans.reduce((acc, current) => [...acc, { location: current.scannedLocation, detail: current.instructions, date: `${current.scanDateTime}` }],[]);
+        return (!response.data.data[0].destination) ? res.json({ result: `Invalid Tracking No. ${trackingId}` }) : res.json({ result: final })
+    })
+    .catch(err => {
+        return res.json({
+            error: err
+        })
     })
 })
 
