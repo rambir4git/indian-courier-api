f069572c0fdfb0aaa71d8c2c333381cdd0e1d395
Rambir <rambir.git@gmail.com>
2020-12-31 22:36:58 +0530

    Update shiprocket.js

--- api/shiprocket.js
+++ api/shiprocket.js
@@ -3,21 +3,22 @@ const router = express.Router();
 const axios = require('axios');
 const tabletojson = require('tabletojson').Tabletojson;
 
-let url = 'https://packaging.shiprocket.in/index.php?route=account/track';
+let trackurl = 'https://packaging.shiprocket.in/index.php?route=account/track';
 
 router.get('/:awb', (req, res) => {
     let trackingId = req.params.awb
-    return axios.post(url,{awbcode:trackingId})
+    var bodyFormData = new FormData();
+    bodyFormData.append('awbcode', trackingId);
+    return axios({
+    method: 'post',
+    url: trackurl,
+    data: bodyFormData,
+    headers: {'Content-Type': 'multipart/form-data' }
+    })
     .then(response => {
         tablesAsJson = tabletojson.convert(response.data);
         console.log(tableAsJson)
-        if (!tablesAsJson || !tablesAsJson.length){
-                return res.json({ result: `Invalid Tracking Id ${trackingId}` })
-            } 
-        else {
-                let modified = tablesAsJson[1].reduce((acc, current) => [...acc, { location: current.Place, detail: current.Status, date: `${current.Date} ${current.Time}` }], [])
-                return res.json({ result: modified })
-            }
+
     })
     .catch(err => {
         return res.json({
