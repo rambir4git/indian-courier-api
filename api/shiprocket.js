c2b7fb17fd8ef923d6b591bed5417f191d1606d7
Rambir <rambir.git@gmail.com>
2020-12-31 22:53:05 +0530

    Update shiprocket.js

--- api/shiprocket.js
+++ api/shiprocket.js
@@ -1,24 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const FormData = require('form-data')
 const tabletojson = require('tabletojson').Tabletojson;
 
-let trackurl = 'https://packaging.shiprocket.in/index.php?route=account/track';
+let url = 'https://packaging.shiprocket.in/index.php?route=account/track';
 
 router.get('/:awb', (req, res) => {
     let trackingId = req.params.awb
-    var bodyFormData = new FormData();
-    bodyFormData.append('awbcode', trackingId);
-    return axios({
-    method: 'post',
-    url: trackurl,
-    data: bodyFormData,
-    headers: {'Content-Type': 'multipart/form-data' }
-    })
+    return axios.post(url,{awbcode:trackingId})
     .then(response => {
-        tablesAsJson = tabletojson.convert(response.data);
-        console.log(tableAsJson)
+        //tablesAsJson = tabletojson.convert(response.data);
+        console.log(response)
         return res.json({status: "bohot sahi"})
 
     })
