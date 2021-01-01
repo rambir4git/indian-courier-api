2e2a5f13eba380e51b37091989d807e290991152
Rambir <rambir.git@gmail.com>
2021-01-01 21:35:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const pickrr = require('./api/pickrr')
 const PORT = process.env.PORT || 5000 
 
 express()
+  .get('/',(request,response)=>{
+    response.status(200).json({
+      build:1,
+      value:500,
+      cost:50
+    })
+  })
   .get('/:trackid', (request, response)=>{
     const trackid = request.params.trackid;
     promise.join(shiprocket(trackid),pickrr(trackid))
